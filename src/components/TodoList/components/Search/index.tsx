bcb3562bcbf448864924a4ceb035758470b7c40a
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-16 23:42:55 -0300

    feat: implement search engine

--- src/components/TodoList/components/Search/index.tsx
+++ src/components/TodoList/components/Search/index.tsx
@@ -1,14 +1,23 @@
 import { FaSearch } from 'react-icons/fa';
+import { ChangeEvent, useContext } from 'react';
 import { SearchContainer, SearchIconWrapper, StyledInputBase } from './style';
+import { TaskContext } from '@/contexts/TaskContext';
 import '@/assets/styles/components/search.sass';
 
 export default function Search(): JSX.Element {
+    const { setSearchTasks } = useContext(TaskContext);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        const searchTask = event.target.value;
+        setSearchTasks(searchTask);
+    };
+
     return (
         <SearchContainer className="header__seach">
             <SearchIconWrapper>
                 <FaSearch />
             </SearchIconWrapper>
-            <StyledInputBase placeholder="Procurar" aria-label="search" />
+            <StyledInputBase onChange={handleChange} placeholder="Procurar" aria-label="search" />
         </SearchContainer>
     );
 }
