76d766f2ff050a7f7938372835bd8b6af435d71b
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-18 23:30:17 -0300

    feat: implementing components

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,4 +1,7 @@
+import { CreateTask } from './components/Tasks/Operations';
 import { Toaster } from '@/components/ui/toaster';
+import Tasks from '@/components/TodoList/components/Tasks';
+import Search from '@/components/TodoList/components/Search';
 import '@/assets/styles/components/todo-list.sass';
 
 export default function TodoList(): JSX.Element {
@@ -11,8 +14,12 @@ export default function TodoList(): JSX.Element {
                         <h2 className="title__content">Todo List</h2>
                     </div>
                 </div>
-                <div className="header__bottom-row"></div>
+                <div className="header__bottom-row">
+                    <CreateTask />
+                    <Search />
+                </div>
             </div>
+            <Tasks />
         </section>
     );
 }
