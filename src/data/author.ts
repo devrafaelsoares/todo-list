956d005c0497cff65691c676a60524930f585ed5
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-22 21:15:59 -0300

    feat: add author data

---
+++ src/data/author.ts
@@ -0,0 +1,7 @@
+import { Author } from '@/types/author';
+
+export const author: Author = {
+    name: 'Rafael Henrique',
+    username: 'devrafaelsoares',
+    github: 'https://github.com/devrafaelsoares',
+};
---
+++ src/types/author.ts
@@ -0,0 +1,5 @@
+export type Author = {
+    readonly name: string;
+    readonly username: string;
+    readonly github: string;
+};
